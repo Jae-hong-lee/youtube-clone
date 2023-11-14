40b29a7be2666dcfbc165b312a95069025b809f3
Jae-hong-Lee <keash8091@gmail.com>
2023-11-14 21:51:04 +0900

    헤더영역작업(1)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <header id="header" role="banner">
-      Header
+      <h1 className="header__logo">
+        <a href="">
+          <em aria-hidden="true"></em>
+          <span>
+            webs
+            <br />
+            youtube
+          </span>
+        </a>
+      </h1>
+      <nav className="header__menu"></nav>
+      <div className="header__sns"></div>
     </header>
   );
 };
