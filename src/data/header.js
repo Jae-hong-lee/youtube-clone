52cf95862d8562804a0e567e970b8a3d37d959b5
Jae-hong-Lee <keash8091@gmail.com>
2023-11-17 04:59:23 +0900

    10. 페이지 SEO 작업

--- src/data/header.js
+++ src/data/header.js
@@ -14,7 +14,7 @@ import { AiOutlineInstagram } from "react-icons/ai";
 
 export const headerMenus = [
   {
-    title: "웹스토리보이",
+    title: "재홍님의 유튜브",
     icon: <CiBaseball />,
     src: "/",
   },
