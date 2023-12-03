054c763e6b0eab2d1a7f0ee97366d9dfddd5c083
Jae-hong-Lee <keash8091@gmail.com>
2023-12-03 11:26:18 +0900

    17. 나머지 페이지 작업

--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -13,12 +13,12 @@ const Developer = () => {
     }, 300);
   }, []);
 
-  const developoerPageClass = loading ? "isLoading" : "isLoaded";
+  const developerPageClass = loading ? "isLoading" : "isLoaded";
 
   return (
     <Main title="추천 개발자" description="오늘의 추천 개발자 유튜버입니다.">
-      <section id="developerPage" className={developoerPageClass}>
-        <h2>😪 추천 개발자를 소개합니다.</h2>
+      <section id="developerPage" className={developerPageClass}>
+        <h2>🥰 오늘의 추천 개발자입니다.</h2>
         <div className="developer__inner">
           {developerText.map((developer, key) => (
             <div className="developer" key={key}>
